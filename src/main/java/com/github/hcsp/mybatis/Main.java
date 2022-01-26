2193bd71aec6d7b3eb6b44925d4b754a34af0eb8
何东升 <he77505357@163.com>
2022-01-26 17:52:01 +0800

    用户批量添加、查询、删除

--- src/main/java/com/github/hcsp/mybatis/Main.java
+++ src/main/java/com/github/hcsp/mybatis/Main.java
@@ -1,17 +1,40 @@
 package com.github.hcsp.mybatis;
 
+import com.github.hcsp.mybatis.entity.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         String resource = "db/mybatis/config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         UserDao userDao = new UserDao(new SqlSessionFactoryBuilder().build(inputStream));
 
-        userDao.getUserByPage(null, 6, 1);
+        // userDao.getUserByPage(null, 6, 1);
+
+        insertTest(userDao);
+    }
+
+    public static void insertTest(UserDao userDao) {
+//        User user1 = new User(null, "hedongsheng", "18584401743", "广西南宁");
+//        User user2 = new User(null, "ds2", "18322222", "广西宾阳");
+//
+//        List<User> userList = new ArrayList<>();
+//        userList.add(user1);
+//        userList.add(user2);
+//
+//        userDao.batchInsertUsers(userList);
+
+        //User user3 = new User(8, null, "18359867458", null);
+
+        //userDao.updateUser(user3);
+        //userDao.deleteUserById(7);
+        userDao.selectUserById(7);
     }
 }
--- src/main/java/com/github/hcsp/mybatis/UserDao.java
+++ src/main/java/com/github/hcsp/mybatis/UserDao.java
@@ -49,6 +49,11 @@ public class UserDao {
      * @param users 待插入的用户列表
      */
     public void batchInsertUsers(List<User> users) {
+        try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
+            Map<String, List> params = new HashMap();
+            params.put("users", users);
+            sqlSession.insert("userBatchInsert", params);
+        }
     }
 
     /**
@@ -57,6 +62,9 @@ public class UserDao {
      * @param user 要修改的用户信息，其id必须不为null
      */
     public void updateUser(User user) {
+        try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
+            sqlSession.update("updateUser", user);
+        }
     }
 
     /**
@@ -65,6 +73,9 @@ public class UserDao {
      * @param id 待删除的用户ID
      */
     public void deleteUserById(Integer id) {
+        try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
+            sqlSession.update("deleteUser", id);
+        }
     }
 
     /**
@@ -74,6 +85,11 @@ public class UserDao {
      * @return 对应的用户
      */
     public User selectUserById(Integer id) {
-        return null;
+        User user;
+        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            user = sqlSession.selectOne("queryUserById", id);
+        }
+        return user;
+
     }
 }
